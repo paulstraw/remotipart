91a49928f9b0ad5c6b490b6e6de8d0b1d935295e
Paul Straw <paulstraw@paulstraw.com>
2014-02-25 13:09:57 -0800

    plz

--- vendor/assets/javascripts/jquery.iframe-transport.js
+++ vendor/assets/javascripts/jquery.iframe-transport.js
@@ -115,10 +115,11 @@
     // and should revert all changes made to the page to enable the
     // submission via this transport.
     function cleanUp() {
-      markers.replaceWith(function(idx) {
-        console.log(markers, files.get(idx), $(files.get(idx)).val());
-        return files.get(idx);
-      });
+      // markers.replaceWith(function(idx) {
+      //   console.log(markers, files.get(idx), $(files.get(idx)).val());
+      //   return files.get(idx);
+      // });
+      console.log(markers);
       form.remove();
       iframe.bind("load", function() { iframe.remove(); });
       iframe.attr("src", "javascript:false;");
